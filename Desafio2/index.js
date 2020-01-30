db950a1608a2d5622b9ab07de1efedb069ba33a6
RodrigoRedivo <rodrigo.a.redivo@gmail.com>
2020-01-30 07:35:01 -0300

    creating challenge2

---
+++ Desafio2/index.js
@@ -0,0 +1,32 @@
+//  CONSTRUÇÃO E IMPRESSÃO DE OBJETOS
+
+const empresa = {
+  nome : 'Rocketseat',
+  cor : 'Roxo',
+  foco : 'Programação',
+  endereco : {
+    rua : 'Rua Guilherme Gembala',
+    numero : 260 
+  },
+}
+
+console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereco.rua}, ${empresa.endereco.numero}`);
+
+// VETORES E OBJETOS
+
+const programador = {
+    nome : 'Carlos',
+    idade : 32,
+    tecnologias: [
+    {
+      nome: 'C++' ,
+      especialidade: 'Desktop', 
+    },
+    {
+      nome: 'JavaScript',
+      especialidade: 'Web/Mobile'
+    }
+  ]
+}
+
+console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade}`);
